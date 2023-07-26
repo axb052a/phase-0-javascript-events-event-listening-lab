a98381473abe48e3fa5722bb7616387ab58e3200
Anthony Beltran <beltrananthony30@icloud.com>
2023-07-25 23:45:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// index.js
 function addingEventListener() {
-}
+    // Get the button element
+    const button = document.getElementById('button');
+  
+    // Add an event listener for the 'click' event
+    button.addEventListener('click', function() {
+      // Code to execute when the button is clicked
+      console.log('Click Me!');
+    });
+  }
+  
+  // Call the function to set up the event listener when the script is loaded
+  addingEventListener();
+  
